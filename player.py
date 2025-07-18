aa370cc6c1eea74460f2e64edf951993da6ec34a
BynJerL <yggcphanq@gmail.com>
2025-07-18 18:18:03 +0700

    add separate player class to ease specific property modification

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, level=1, point=0, currentHealth=150, maxHealth=150, power=20, currentExp=0, limitExp=150):
+        self.name = name
+        self.level = level
+        self.point = point
+        self.currentHealth = currentHealth
+        self.maxHealth = maxHealth
+        self.power = power
+        self.currentExp = currentExp
+        self.limitExp = limitExp
--- prototype_app.py
+++ prototype_app.py
@@ -1,21 +1,23 @@
+from player import Player
+
 class MainApp:
     def __init__(self):
-        self.playerName = None
-        self.playerLevel = None
-        self.playerPoint = None
+        self.playerData = None
 
     def initialRun(self):
-        self.playerName = input("Insert your name: ")
-        self.playerLevel = 1
-        self.playerPoint = 0
+        playerName = input("Insert your name: ")
+        self.playerData = Player(name=playerName)
 
         print("Your current status:")
         self.printPlayerStatus()
     
     def printPlayerStatus(self):
-        print(f"playerName: {self.playerName}")
-        print(f"playerLevel: {self.playerLevel}")
-        print(f"playerPoint: {self.playerPoint}")
+        print(f"playerName: {self.playerData.name}")
+        print(f"playerLevel: {self.playerData.level}")
+        print(f"playerPoint: {self.playerData.point}")
+        print(f"playerExp: {self.playerData.currentExp}/{self.playerData.limitExp}")
+        print(f"playerHp: {self.playerData.currentHealth}/{self.playerData.maxHealth}")
+        print(f"playerPower: {self.playerData.power}")
 
 if __name__ == "__main__":
     root = MainApp()
